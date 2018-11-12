2c6851a5aa6d9c7f7ad5e1dd5347858f506aada4
Lars Tveito <larstvei@ifi.uio.no>
2018-11-12 14:08:12 +0100

    Include clj-http and make visualize-traces-clj.core the entry point
    clj-http should have been included in e249b08.

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [quil "2.7.1"]])
+                 [quil "2.7.1"]
+                 [clj-http "3.9.1"]]
+  :main visualize-traces-clj.core
+  :aot  [visualize-traces-clj.core])

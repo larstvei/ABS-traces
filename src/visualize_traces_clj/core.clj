74c9462b919598f01ed72b21706e8e0fb1f4c513
Lars Tveito <larstvei@ifi.uio.no>
2019-01-08 11:51:04 +0100

    Add keybinding for advancing the simulator clock
    And also clean up the key-handler a little bit.

--- src/visualize_traces_clj/core.clj
+++ src/visualize_traces_clj/core.clj
@@ -21,29 +21,41 @@
         :body json-str->trace)
     (catch Exception e nil)))
 
+(defn advance-simulator-clock
+  ([] (advance-simulator-clock 1))
+  ([n] (try
+         (-> (str "http://localhost:8080/clock/advance?by=" n)
+             (h/get {:accept :json}))
+         (catch Exception e nil))))
+
 (defn key-handler [state event]
-  (let [n (count (:history state))
-        state (case (q/key-as-keyword)
-                :? (update state :help not)
+  (q/redraw)
+  (let [n (count (:history state))]
+    (case (q/key-as-keyword)
+      :? (update state :help not)
 
-                :up (update state :start (comp (partial max 0) dec))
+      :up (update state :start (comp (partial max 0) dec))
 
-                :down (let [m (- n (:height state))]
-                        (update state :start (comp (partial min m) inc)))
+      :down (let [m (- n (:height state))]
+              (update state :start (comp (partial min m) inc)))
 
-                :- (update state :height (comp (partial max 1) dec))
+      :- (update state :height (comp (partial max 1) dec))
 
-                :+ (update state :height (comp (partial min n) inc))
+      :+ (update state :height (comp (partial min n) inc))
 
-                := (update state :height (comp (partial min n) inc))
+      := (update state :height (comp (partial min n) inc))
 
-                :l (if-let [new-trace (get-trace-from-simulator)]
-                     (setup new-trace)
-                     state)
+      :l (if-let [new-trace (get-trace-from-simulator)]
+           (setup new-trace)
+           state)
 
-                state)]
-    (q/redraw)
-    state))
+      :c (do (advance-simulator-clock)
+             (let [t (get-trace-from-simulator)]
+               (merge state {:trace t
+                             :history (trace->history t)
+                             :cogs (keys t)})))
+
+      state)))
 
 (defn ^:export run-sketch []
   (q/defsketch visualize-traces-clj
